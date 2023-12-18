2c8a358a0613d2a17515d3a2e578067f1363de7b
Aaron <aaron26@uw.edu>
2023-12-17 20:47:31 -0800

    removed example file,implemented methods, removed prompt added comments

--- buffer.c
+++ buffer.c
@@ -21,46 +21,73 @@
 #include <string.h>
 #include "buffer.h"
 
-//define the struct for the bounded buffer
-//implmentation using pthreads and bounded buffers
-//actually looked at buffer.h......
-
-
-
 // Initialize the bounded buffer
 //Memory should be allocated for the bounded buffer using malloc()
 void initbuff(int nslots, buffer_t *buff, char * n)
-{ 
-  //pointer to contents of bounded buffer
-  buff ->buffer = (int *) malloc(nslots * sizeof(int));
-  //size of bounded buffer
-  buff-> maxSlots = nslots;
-  buff ->use_ptr = 0;
-  buff ->fill_ptr = 0;
-  buff ->name = n;
-  //initialize mutex
+{
+  buff->buffer = malloc(sizeof(int) * nslots);
+  buff->maxSlots = nslots;
+  buff->use_ptr = 0;
+  buff->fill_ptr = 0;
+  buff->count = 0;
+  buff->name = n;
+  //initialize the mutex and condition variables
   pthread_mutex_init(&buff->mutex, NULL);
-  //initialize condition variables
   pthread_cond_init(&buff->cond, NULL);
 
 }
-
 //method to release memory resources used by the bounded buffer using free()
 void freebuff(int nslots, buffer_t *buff, char * n)
 {
+  free(buff->buffer);
+  pthread_mutex_destroy(&buff->mutex);
+  pthread_cond_destroy(&buff->cond);
 
 }
-
 //method used to load a new value into the bounded buffer
-//combines put() and producer() into one method
 void put(int value, buffer_t *buff)
 {
+  //lock the mutex 
+  pthread_mutex_lock(&buff->mutex);
+  //wait until there is space in the buffer
+  while(buff->count == buff->maxSlots)
+  {
+    pthread_cond_wait(&buff->cond, &buff->mutex);
+  }
+  //put the item in the buffer
+  buff->buffer[buff->fill_ptr] = value;
+  //increment the fill pointer
+  buff->fill_ptr = (buff->fill_ptr + 1) % buff->maxSlots;
+  //increment the count
+  buff->count++;
+  //signal that the buffer is no longer empty
+  pthread_cond_signal(&buff->cond);
+  //unlock the mutex
+  pthread_mutex_unlock(&buff->mutex);
   
 }
-
-//method used to retrieve a value from the bounded buffer
+//method used to get a value from the bounded buffer
 int get(buffer_t *buff)
 {
-  return -1; 
+  //lock the mutex
+  pthread_mutex_lock(&buff->mutex);
+  //wait until there is something in the buffer
+  while(buff->count == 0)
+  {
+    pthread_cond_wait(&buff->cond, &buff->mutex);
+  }
+  //get the item from the buffer
+  int value = buff->buffer[buff->use_ptr];
+  //increment the use pointer
+  buff->use_ptr = (buff->use_ptr + 1) % buff->maxSlots;
+  //decrement the count
+  buff->count--;
+  //signal that the buffer is no longer full
+  pthread_cond_signal(&buff->cond);
+  //unlock the mutex
+  pthread_mutex_unlock(&buff->mutex);
+  //return the item
+  return value;
+  
 }
 
